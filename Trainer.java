06d67042386eb145fac35eb61630249ac4cbee91
Ryan Oman <namos@gregkh.cs.washington.edu>
2010-10-04 10:49:59 -0700

    test

--- Trainer.java
+++ Trainer.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
+//
 public class Trainer{
 	public static final Integer ACTOR = 0;
 	public static final Integer BOOK = 1;
