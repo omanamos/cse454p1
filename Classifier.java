2873e2bd832dc4eab5e0ddac15f3d56455cac8bc
Gerhalt <gerhalt@cs.washington.edu>
2010-10-11 16:50:59 -0700

    Added comments

--- Classifier.java
+++ Classifier.java
@@ -2,21 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Classifier {
-	Trainer known;
-
+	
+	/**
+	 * Determines what class a file most likely fits, based on known data
+	 * 
+	 * @param t	trained data for comparison
+	 * @param f	file to check
+	 * @return	integer representation of the class the file most likely matches
+	 */
 	public static int getClass(Trainer t, File f) {
 		return Utils.maxInd(scoreClass(t, f));
 	}
 	
+	/**
+	 * Takes an input file and gets all the unique tokens
+	 * 
+	 * @param f	file
+	 * @return	list of unique tokens
+	 */
 	private static List<String> getTokens(File f) {
 		List<String> tokens = new ArrayList<String>();
 		
 		try {
+			// Get unique tokens from file
 	        Scanner s = new Scanner(f);
-	        
 	        while(s.hasNextLine()) {
 	        	for(String token : s.nextLine().split(" ")) {
-	        		tokens.add(token);
+	        		if(!tokens.contains(s))
+	        			tokens.add(token);
 	        	}
 	        }
 		} catch(FileNotFoundException fe) {
@@ -25,23 +38,28 @@ public class Classifier {
         return tokens;
 	}
 	
+	/**
+	 * Implementation of the getClass method
+	 * 
+	 * @param t	training data
+	 * @param f	file
+	 * @return	array of values, indexed by class
+	 */
 	private static Double[] scoreClass(Trainer t, File f) {
-		List<String> tokens = new ArrayList<String>();
-		for(String s : getTokens(f)) {
-			if(!tokens.contains(s))
-				tokens.add(s);
-		}
+		List<String> tokens = getTokens(f);
+
+		// for each c in C
 		
-		
+		// score[c] <- log prior[c]
 		Double[] score = new Double[t.prior.length];
 		for(int i = 0; i < t.prior.length; i++)
 			score[i] = Math.log(t.prior[i]);
 		
-		
-		
 		for(int i = 0; i < t.CLASSES.length; i++) {
+			// for each t in W
 			for(String token : tokens) {
-				if(t.vocab.containsKey(token)) { 
+				if(t.vocab.containsKey(token)) { // make sure we actually saw the token when we were checking the class 
+					// score[c] += log condprob[t][c]
 					score[i] += Math.log(t.condProb.get(token).get(i));
 				}
 			}
